7ba44805c3d2d7756598cac20ed35bbc61d329c9
george <george@no.email>
2019-12-02 19:17:34 -0500

    We have a button!

--- xfig-3.2.7b/src/e_send_to_back.c
+++ xfig-3.2.7b/src/e_send_to_back.c
@@ -12,7 +12,7 @@
 #include "u_markers.h"
 #include "u_undo.h"
 #include "w_canvas.h"
-//#include "w_modepanel.h"
+#include "w_modepanel.h"
 #include "w_mousefun.h"
 #include "w_msgpanel.h"
 
@@ -26,17 +26,18 @@
 void send_to_back_selected(void)
 {
     //FROM e_movept.c (same pointer)
-    set_mousefun("move point", "horiz/vert move", "", LOC_OBJ, LOC_OBJ, LOC_OBJ);
-    canvas_kbd_proc = null_proc;
-    canvas_locmove_proc = null_proc;
-    canvas_ref_proc = null_proc;
-    //init_searchproc_left(init_arb_move_point);
-    //init_searchproc_middle(init_stretch_move_point);
-    canvas_leftbut_proc = point_search_left;
-    canvas_middlebut_proc = point_search_middle;
-    canvas_rightbut_proc = null_proc;
-    set_cursor(pick9_cursor);
-    force_anglegeom();
-    reset_action_on();
+    // set_mousefun("move point", "horiz/vert move", "", LOC_OBJ, LOC_OBJ, LOC_OBJ);
+    // canvas_kbd_proc = null_proc;
+    // canvas_locmove_proc = null_proc;
+    // canvas_ref_proc = null_proc;
+    // //init_searchproc_left(init_arb_move_point);
+    // //init_searchproc_middle(init_stretch_move_point);
+    // canvas_leftbut_proc 
+    // = point_search_left;
+    // canvas_middlebut_proc = point_search_middle;
+    // canvas_rightbut_proc = null_proc;
+    // set_cursor(pick9_cursor);
+    // force_anglegeom();
+    // reset_action_on();
     return;
 }
