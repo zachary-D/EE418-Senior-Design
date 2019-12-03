1d216460527580fed9dac46f2f4f380c6f00f3db
george <george@no.email>
2019-12-02 19:54:14 -0500

    Basic send to back functionality implemented

--- xfig-3.2.7b/src/e_send_to_back.c
+++ xfig-3.2.7b/src/e_send_to_back.c
@@ -22,6 +22,8 @@
 #include "w_cursor.h"
 #include "w_util.h"
 
+static void send_back(F_line *obj, int type, int x, int y, F_point *p, F_point *q, int pnum);
+
 
 void send_to_back_selected(void)
 {
@@ -30,13 +32,31 @@ void send_to_back_selected(void)
      canvas_kbd_proc = null_proc;
      canvas_locmove_proc = null_proc;
      canvas_ref_proc = null_proc;
+
+
+    canvas_leftbut_proc = object_search_left;
+    //canvas_middlebut_proc = point_search_middle;
+    init_searchproc_left(send_back);
+    //init_searchproc_middle(delete_arrow_head);
     // //init_searchproc_left(init_arb_move_point);
     // //init_searchproc_middle(init_stretch_move_point);
     // canvas_leftbut_proc 
     // = point_search_left;
-     canvas_middlebut_proc = null_proc;
+     //canvas_middlebut_proc = null_proc;
      canvas_rightbut_proc = null_proc;
      set_cursor(pick9_cursor);
      force_anglegeom();
      reset_action_on();
 }
+
+static void send_back(F_line *obj, int type, int x, int y, F_point *p, F_point *q, int pnum)
+{
+    if (obj->depth == 999) {
+        put_msg("999 depth");
+    }
+
+    remove_depth(type, obj->depth); //manages the depth manager
+    obj->depth = 998;
+	add_depth(type, obj->depth);
+}
+
